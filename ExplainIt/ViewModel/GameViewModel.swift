7dd39962d846a745d88d31fc1ac2c64b8ff6e63b
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-01-25 13:13:12 +0200

    write func for save and load data

--- ExplainIt/ViewModel/GameViewModel.swift
+++ ExplainIt/ViewModel/GameViewModel.swift
@@ -142,10 +142,35 @@ extension GameViewModel {
         let encoder = JSONEncoder()
         if let encodedTeams = try? encoder.encode(teams),
            let encodedTeamPoints = try? encoder.encode(teamPoints),
-           let encodedTeamRounds = try? encoder.encode(teamRounds) {
+           let encodedTeamRounds = try? encoder.encode(teamRounds),
+           let encodedRoundTime = try? encoder.encode(roundTime),
+           let encodedRequiredPoints = try? encoder.encode(requiredPoints)
+        {
             UserDefaults.standard.set(encodedTeams, forKey: "teams")
             UserDefaults.standard.set(encodedTeamPoints, forKey: "teamPoints")
             UserDefaults.standard.set(encodedTeamRounds, forKey: "teamRounds")
+            UserDefaults.standard.set(encodedRoundTime, forKey: "roundTime")
+            UserDefaults.standard.set(encodedRequiredPoints, forKey: "requiredPoints")
+        }
+    }
+    
+    func loadGameData() {
+        let decoder = JSONDecoder()
+        if let teamsData = UserDefaults.standard.data(forKey: "teams"),
+           let teamPointsData = UserDefaults.standard.data(forKey: "teamPoints"),
+           let teamRoundsData = UserDefaults.standard.data(forKey: "teamRounds"),
+           let roundTimeData = UserDefaults.standard.data(forKey: "roundTime"),
+           let requiredPointsData = UserDefaults.standard.data(forKey: "requiredPoints"),
+           let loadedTeams = try? decoder.decode([String].self, from: teamsData),
+           let loadedTeamPoints = try? decoder.decode([String: Int].self, from: teamPointsData),
+           let loadedTeamRounds = try? decoder.decode([String: Int].self, from: teamRoundsData),
+           let loadedRoundTime = try? decoder.decode(Int.self, from: roundTimeData),
+           let loadedRequiredPoints = try? decoder.decode(Int.self, from: requiredPointsData) {
+            teams = loadedTeams
+            teamPoints = loadedTeamPoints
+            teamRounds = loadedTeamRounds
+            roundTime = loadedRoundTime
+            requiredPoints = loadedRequiredPoints
         }
     }
 }
