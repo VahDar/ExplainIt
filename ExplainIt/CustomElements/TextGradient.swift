390e922d183404d7a05f218df15f2689678f5e8c
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-02-09 17:02:00 +0200

    some disegne changed

---
+++ ExplainIt/CustomElements/TextGradient.swift
@@ -0,0 +1,18 @@
+//
+//  TextGradient.swift
+//  ExplainIt
+//
+//  Created by Vakhtang on 09.02.2024.
+//
+
+import SwiftUI
+
+struct TextGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextGradient()
+}
