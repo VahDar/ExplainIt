5d0a88f01fd7512289b227977c59a50342b76967
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2024-02-23 20:02:22 +0200

    delete some staff

--- ExplainIt/Views/TimerView.swift
+++ ExplainIt/Views/TimerView.swift
@@ -1,104 +1,15 @@
-////
-////  TimerView.swift
-////  ExplainIt
-////
-////  Created by Vakhtang on 07.09.2023.
-////
 //
-//import SwiftUI
-//import AVFoundation
+//  TimerView.swift
+//  ExplainIt
 //
-//struct TimerView: View {
-//    @State private var timerValue: CGFloat = 0.0
-//    @Binding var isTimerRunning: Bool
-//    @EnvironmentObject var viewModel: GameViewModel
-//    var timerDuration: TimeInterval
-//    var onTimerEnd: () -> Void
-//    @State private var audioPlayer: AVAudioPlayer?
-//    @State private var timer: Timer?
-//    
-//    var body: some View {
-//        VStack {
-//            ZStack {
-//                RoundedRectangle(cornerRadius: 20)
-//                    .trim(from: 0, to: timerValue)
-//                    .stroke(Color(red: 79/255, green: 74/255, blue: 183/255), lineWidth: 10)
-//                    .frame(width: 400, height: 300)
-//                    .rotationEffect(.degrees(-90))
-//                    .onAppear() {
-//                        startTimer()
-//                    }
-//            }
-//            .onAppear {
-//                prepareAudioPlayer()
-//            }
-//        }
-//    }
-//            func startTimer() {
-//                isTimerRunning = true
-//                var isSoundPlayed = false
-//                
-//                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-//                    
-//                    if timerValue < 1.0 {
-//                        withAnimation(.linear(duration: 1.0)) {
-//                            timerValue += 1.0 / timerDuration
-//                        }
-//                        
-//                        let timerLeft = timerDuration - (CGFloat(timerValue) * timerDuration)
-//                        if viewModel.isSoundEnabled && timerLeft <= 5 && !isSoundPlayed {
-//                            audioPlayer?.play()
-//                            isSoundPlayed = true
-//                        }
-//                    } else {
-//                        stopTimer()
-//                        onTimerEnd()
-//                    }
-//                }
-//                if let timer = timer {
-//                    RunLoop.current.add(timer, forMode: .common)
-//                }
-//            }
-//        
-//    
-//    private func prepareAudioPlayer() {
-//        guard let soundURL = Bundle.main.url(forResource: "sound", withExtension: "mp3") else { return }
-//        do {
-//            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-//            audioPlayer?.prepareToPlay()
-//        } catch {
-//            print("Error with download audio file")
-//        }
-//    }
-//    
-//    private func stopTimer() {
-////        timer?.invalidate()
-////        timer = nil
-//        isTimerRunning = false
-//        onTimerEnd()
-//    }
-//}
+//  Created by Vakhtang on 07.09.2023.
 //
-//
-//
-//struct TimerView_Previews: PreviewProvider {
-//    @State static var isTimerRunning = true
-//    @State static var isPaused = false
-//    @State static var timerDuration = 30
-//    static var previews: some View {
-//        TimerView(isTimerRunning: $isTimerRunning, timerDuration: TimeInterval(timerDuration), onTimerEnd: {})
-//    }
-//}
 
 import SwiftUI
 import UserNotifications
 import AVFoundation
 
-// MARK: - ContentView.swift изменений не требуется, так как TimerView будет обновлен
 
-// MARK: - GameViewModel.swift изменений не требуется, так как он уже включает необходимую логику
-
-// MARK: - TimerView.swift
 struct TimerView: View {
     @State private var to: CGFloat = 0
     @State private var count = 0
@@ -135,10 +46,6 @@ struct TimerView: View {
                     Image(systemName: "pause.circle")
                         .foregroundColor(.blue)
                         .font(.largeTitle)
-//                        .padding(.vertical, 10)
-//                        .frame(width: 60)
-//                        .background(Color(red: 79/255, green: 74/255, blue: 183/255))
-//                        .clipShape(Circle())
                 }
                 .padding()
                 .offset(x: -100, y: 100)
@@ -171,11 +78,6 @@ struct TimerView: View {
         timerPaused = true
         isTimerRunning = false
     }
-
-//    private func resumeTimer() {
-//        timerPaused = false
-//        isTimerRunning = true
-//    }
 
     private func prepareAudioPlayer() {
         guard let soundURL = Bundle.main.url(forResource: "sound", withExtension: "mp3") else { return }
